fb75ba8a15731380dcfd87a96751330cf14c2f5d
Abhishek Sonwal <abhisheksonwal@gmail.com>
2025-02-15 10:49:29 +0000

    some chnages

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,6 +6,7 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      poolSize: 10
     });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
