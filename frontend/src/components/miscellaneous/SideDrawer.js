2de545443cc4108f14031876fbc0289125d8336a
Abhishek Sonwal <abhisheksonwal@gmail.com>
2025-02-22 10:48:52 +0000

    title change

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -142,7 +142,7 @@ function SideDrawer() {
           </Button>
         </Tooltip>
         <Text fontSize="2xl" fontFamily="Work sans">
-          Talk-A-Tive
+        Abhishek Talk-a-tive
         </Text>
         <div>
           <Menu>
